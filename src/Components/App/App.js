e55ab232eb62e9d17e771d8ae0a19b4e71159b20
fortitudeman <miraclecreator2050@gmail.com>
2020-09-05 04:58:29 +0800

    add footer and content component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import Title from '../Title/title'
-import { Table } from './styles';
+import Title from '../Title/title';
+import Content from '../Content/content';
+import Footer from '../Footer/footer';
+import { Table, Space } from './styles';
 class App extends Component {
   render() {
     return (
@@ -8,6 +10,9 @@ class App extends Component {
         <Table>
           <tbody>
             <Title />
+            <Space />
+            <Content />
+            <Footer />
           </tbody>
         </Table>
       </center>
