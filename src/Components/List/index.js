b507cf9528a1f9045c5e5b11c841de55c479259f
fortitudeman <miraclecreator2050@gmail.com>
2020-09-05 08:43:28 +0800

    first page build

---
+++ src/Components/List/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import ListItem from '../ListItem';
+
+import { ListWrapper } from './styles';
+
+class List extends Component {
+  static propTypes = {
+    stories: PropTypes.array.isRequired,
+  };
+
+  render() {
+    const { stories } = this.props;
+    return (
+      <ListWrapper>
+        {stories.map(story => (
+          <ListItem key={story.id} {...story} />
+        ))}
+      </ListWrapper>
+    )
+  }
+}
+export default List;
---
+++ src/Components/List/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ListWrapper = styled.tr`
+  border-radius: 4px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+`;
