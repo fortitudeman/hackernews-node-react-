57eeafcf2028ff2f665bf940699b0203356981b3
fortitudeman <miraclecreator2050@gmail.com>
2020-09-05 08:39:11 +0800

    first page build

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Title from '../Title/title';
 import Content from '../Content/content';
 import Footer from '../Footer/footer';
 import { Table, Space } from './styles';
+
 class App extends Component {
+  static defaultProps = {
+    stories: [],
+  };
+
+  static propTypes = {
+    stories: PropTypes.array.isRequired,
+    fetchStoriesFirstPage: PropTypes.func.isRequired,
+  }
+  componentDidMount() {
+    this.props.fetchStoriesFirstPage();
+  }
+
   render() {
+    const { stories } = this.props;
     return (
       <center>
         <Table>
           <tbody>
             <Title />
             <Space />
-            <Content />
+            <Content stories={stories}/>
             <Footer />
           </tbody>
         </Table>
